68a91d39428865848aa3fe81baf4ba7f8fe5119e
world-beater <yhh915160157@163.com>
2021-08-17 06:32:40 +0800

    city router

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+    <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+.header
+  position relative
+  height: 0.86rem;
+  line-height: 0.86rem;
+  overflow: hidden;
+  text-align: center;
+  color: #fff;
+  font-size: 0.32rem;
+  background: $bgColor;
+  .header-back
+    position absolute
+    width 0.64rem
+    top 0
+    left 0
+    text-align center
+    font-size .4rem
+    color #fff
+</style>
