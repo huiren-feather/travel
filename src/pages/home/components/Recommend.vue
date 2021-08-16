420f539bcd629ca8e1b0cb36ea41c15c8b8e4a18
world-beater <yhh915160157@163.com>
2021-08-16 14:12:18 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl" alt />
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: '//imgs.qunarzz.com/p/p53/1809/59/a95e0544d16dc102.jpg_256x160_74bab347.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '阿汤哥《碟中谍》取景地'
+      }, {
+        id: '0002',
+        imgUrl: '//imgs.qunarzz.com/p/p53/1809/59/a95e0544d16dc102.jpg_256x160_74bab347.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '阿汤哥《碟中谍》取景地'
+      }, {
+        id: '0003',
+        imgUrl: '//imgs.qunarzz.com/p/p53/1809/59/a95e0544d16dc102.jpg_256x160_74bab347.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '阿汤哥《碟中谍》取景地'
+      }, {
+        id: '0004',
+        imgUrl: '//imgs.qunarzz.com/p/p53/1809/59/a95e0544d16dc102.jpg_256x160_74bab347.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '阿汤哥《碟中谍》取景地'
+      }]
+    }
+  }
+
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl';
+
+.title {
+  margin-top: 0.2rem;
+  line-height: 0.8rem;
+  background: #eee;
+  text-indent: 0.2rem;
+}
+
+.item {
+  overflow: hidden;
+  display: flex;
+  height: 1.9rem;
+
+  .item-img {
+    width: 1.7rem;
+    height: 1.7rem;
+    padding: 0.1rem;
+  }
+
+  .item-info {
+    flex: 1;
+    padding: 0.1rem;
+    min-width: 0;
+
+    .item-title {
+      line-height: 0.54rem;
+      font-size: 0.32rem;
+    }
+
+    .item-desc {
+      line-height: 0.4rem;
+      color: #ccc;
+      ellipsis();
+    }
+
+    .item-button {
+      line-height: 0.44rem;
+      margin-top: 0.2rem;
+      background: #ff9300;
+      padding: 0 0.2rem;
+      border-radius: 0.06rem;
+      color: #fff;
+    }
+  }
+}
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" alt />
+        </div>
+
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <!-- <button class="item-button">查看详情</button> -->
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data() {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: '//imgs.qunarzz.com/vs_ceph_vcimg/79faa5a73731e84a7731db49d5baa91e.jpeg',
+        title: '大连圣亚海洋世界',
+        desc: '阿汤哥《碟中谍》取景地'
+      }, {
+        id: '0002',
+        imgUrl: '//imgs.qunarzz.com/p/p53/1809/59/a95e0544d16dc102.jpg_256x160_74bab347.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '阿汤哥《碟中谍》取景地'
+      }, {
+        id: '0003',
+        imgUrl: '//imgs.qunarzz.com/p/p53/1809/59/a95e0544d16dc102.jpg_256x160_74bab347.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '阿汤哥《碟中谍》取景地'
+      }, {
+        id: '0004',
+        imgUrl: '//imgs.qunarzz.com/p/p53/1809/59/a95e0544d16dc102.jpg_256x160_74bab347.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '阿汤哥《碟中谍》取景地'
+      }]
+    }
+  }
+
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl';
+
+.title {
+  margin-top: 0.2rem;
+  line-height: 0.8rem;
+  background: #eee;
+  text-indent: 0.2rem;
+}
+.item-img-wrapper
+  overflow hidden
+  height 0
+  padding-bottom 33.9%
+  .item-img {
+    width  100%
+
+  }
+.item-info {
+  padding: 0.1rem;
+  .item-title {
+    line-height: 0.54rem;
+    font-size: 0.32rem;
+  }
+  .item-desc {
+    line-height: 0.4rem;
+    color: #ccc;
+    ellipsis();
+  }
+}
+</style>
